84ccd1970d69bd685bb6d2820e4ad5d63015c9c2
urmom999999 <alvarodelvalled@gamil.com>
2025-11-13 17:23:22 +0100

    Funciona?

--- api/server.js
+++ api/server.js
@@ -8,12 +8,14 @@ app.use(express.json());
 
 //LOCAL!!
 //mongoose.connect('mongodb+srv://Admin:Admin420420@backenddb.f4b02ek.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+//MONGO_URI=mongodb://admin:adminadmin@mongodb:27017/test_base_local?authSource=admin
+const MONGO_URI= process.env.MONGO_URI || 'mongodb://admin:adminadmin@mongodb:27017/test_base_local?authSource=admin';
 
 const TestUsuario= new mongoose.Schema({
     nombre:String,
     apellido:String})
    const Usuario= mongoose.model('Usuario',TestUsuario);
-
+//GET USUARIOS
 app.get('/usuarios', async (req, res) => {
   try {
     const usuarios = await Usuario.find();
@@ -22,6 +24,10 @@ app.get('/usuarios', async (req, res) => {
     res.json({ error: error.message });
   }
 });
+//NEW USUARIO
+
+
+
 
 //http://localhost:5000
 //INICIAR 5000
