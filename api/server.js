27009e087b912dd597ef8a5c9168ce698e0348a7
urmom999999 <alvarodelvalled@gamil.com>
2025-11-13 16:53:21 +0100

    Local?

--- api/server.js
+++ api/server.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-//Con ip no funciona
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://Admin:Admin420420@backenddb.f4b02ek.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
+//LOCAL!!
+//mongoose.connect('mongodb+srv://Admin:Admin420420@backenddb.f4b02ek.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
 
 const TestUsuario= new mongoose.Schema({
     nombre:String,
